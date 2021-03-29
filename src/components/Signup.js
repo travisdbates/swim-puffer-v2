a75f67bef1ffb0beee8bf0483f485a94497ebc83
travisdbates <travisbatesdev@gmail.com>
2021-03-29 09:00:02 -0700

    removing 1.5 from age selection

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -449,7 +449,6 @@ class Signup extends Component {
                                         <em>Select an option</em>
                                       </MenuItem>
                                       <MenuItem value={1}>1</MenuItem>
-                                      <MenuItem value={1.5}>18 months</MenuItem>
                                       <MenuItem value={2}>2</MenuItem>
                                       <MenuItem value={3}>3</MenuItem>
                                       <MenuItem value={4}>4</MenuItem>
